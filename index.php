e576616f7a0c9b7afa78a21a737747140560800f
ryannthegr8 <mmbaleryan@gmail.com>
2023-12-18 22:35:39 +0300

    making website

--- index.php
+++ index.php
@@ -5,17 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Management System</title>
     <link rel="stylesheet" href="./style.css">
+
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+    
 </head>
 <body>
     <nav>
-        <label class="logo">R-System</label>
+        <label class="logo">R-SYSTEM</label>
         <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Contact</a></li>
-            <li><a href="">Admission</a></li>
-            <li><a href="">Login</a></li>
+            <li><a href="">HOME</a></li>
+            <li><a href="">CONTACT</a></li>
+            <li><a href="">ADMISSION</a></li>
+            <li><a href="" class="btn btn-success">LOGIN</a></li>
         </ul>
     </nav>
-    <h1>You may sleep bro</h1>
+    <div class="section1">
+        <label for="" class="img_text"> We teach students we care</label>
+        <img class="main_img" src="./assets/school_management.jpg" alt="Background Image">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                i
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
