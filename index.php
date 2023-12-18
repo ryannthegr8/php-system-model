d4df0123cb01532b9cc176a76c72ad34d87bb223
ryannthegr8 <mmbaleryan@gmail.com>
2023-12-18 16:19:52 +0300

    initial making website

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <nav>
+        <label class="logo">R-System</label>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+    </nav>
+    <h1>You may sleep bro</h1>
+</body>
+</html>
