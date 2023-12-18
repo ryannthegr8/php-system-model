38c728b201185c6c17ffff58b02706ddd2bc84a6
Dmokay <ddmokayy@gmail.com>
2023-12-19 01:12:19 +0300

    making website done

--- index.php
+++ index.php
@@ -28,10 +28,99 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4">
-                i
+                <img src="./assets/school2.jpg" class="" alt="">
+            </div>
+            <div class="col-md-8">
+                <h1>Welcome to school</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex ipsa, dolorum corrupti quos ratione exercitationem ab sunt quam maiores, praesentium placeat doloribus doloremque sit tempora non animi consequatur recusandae saepe!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptatum quos aliquid fuga libero aspernatur voluptatem porro ad deserunt error impedit repellendus consectetur possimus tempora non, ipsum quam facilis! Sed?
+                </p>
             </div>
         </div>
     </div>
+
+    <center>
+        <h1 class="heading">Our Teachers</h1>
+    </center>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                 <img class="teacher" src="./assets/teacher1.jpg" alt="">
+                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam impedit adipisci quidem dolor quia harum incidunt. Tempora labore dignissimos, eveniet voluptatum culpa nihil. Rerum eaque animi dicta quo magni adipisci!</p>
+                
+                </div>
+            <div class="col-md-4">
+                <img class="teacher" src="./assets/teacher2.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam impedit adipisci quidem dolor quia harum incidunt. Tempora labore dignissimos, eveniet voluptatum culpa nihil. Rerum eaque animi dicta quo magni adipisci!</p>
+            
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="./assets/teacher3.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam impedit adipisci quidem dolor quia harum incidunt. Tempora labore dignissimos, eveniet voluptatum culpa nihil. Rerum eaque animi dicta quo magni adipisci!</p>
+            
+            </div>
+        </div>
+    </div>
+
+    <center>
+        <h1 class="heading">Our Coursees</h1>
+    </center>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                 <img class="teacher" src="./assets/web.jpg" alt="">
+                 <h3>Web developer</h3>
+                
+                </div>
+            <div class="col-md-4">
+                <img class="teacher" src="./assets/graphic.jpg" alt="">
+                <h3>Graphic Designer</h3>
+            
+            </div>
+            <div class="col-md-4">
+                <img class="teacher" src="./assets/marketing.png" alt="">
+                <h3>Marketing</h3>
+            
+            </div>
+        </div>
+    </div>
+
+    <center>
+        <h1 class="heading">Admission Form</h1>
+    </center>
+
+    <div class="admission_form" align="center">
+        <form action="">
+            <div>
+                <label for="name" class="label_text">Name</label>
+                <input class="input_deg" type="text" name="name">
+            </div>
+            <div>
+                <label for="mail" class="label_text">Email</label>
+                <input class="input_deg" type="text" name="mail">
+            </div>
+            <div>
+                <label for="phone" class="label_text">Phone</label>
+                <input class="input_deg" type="text" name="phone">
+            </div>
+            <div>
+                <label for="message" class="label_text">Message</label>
+                <textarea class="input_txt" name="message" id="" cols="30" rows="10"></textarea>
+            </div>
+            <div>
+                <input class="btn btn-primary" id="submit" type="submit" value="Apply">
+            </div>
+        </form>
+    </div>
+
+    <footer>
+        <h2 class="footer_text"> All @copyright reserved by ryan mmbale</h2>
+    </footer>
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
